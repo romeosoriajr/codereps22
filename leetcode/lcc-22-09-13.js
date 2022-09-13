242c32b322280bb236885dae3e52ffdc7e1b349a
Romeo Soria Jr <rsj@rome.af>
2022-09-13 11:18:38 -0700

    leetcode daily

---
+++ leetcode/lcc-22-09-13.js
@@ -0,0 +1,23 @@
+function validUtf8(data) {
+  let remaining = 0;
+
+  for (let x of data) {
+    if (remaining === 0) {
+      if (x >> 5 == 0b110) {
+        remaining = 1;
+      } else if (x >> 4 == 0b1110) {
+        remaining = 2;
+      } else if (x >> 3 == 0b11110) {
+        remaining = 3;
+      } else if (x >> 7 != 0) {
+        return false;
+      }
+    } else {
+      if (x >> 6 != 0b10) return false;
+      else remaining--;
+    }
+  }
+
+  return remaining == 0;
+}
+
