c91cf49a2dc0dd85d3b588c63f6f97c76f0341fc
Romeo Soria Jr <rsj@rome.af>
2022-09-29 10:06:41 -0700

    leetcode daily

---
+++ leetcode/lcc-22-09-29.js
@@ -0,0 +1,16 @@
+function findClosestElements(arr, k, x) {
+  let left = 0;
+  let right = arr.length - k;
+
+  while (left < right) {
+    const mid = left + Math.floor((right-left) /2);
+    if (x - arr[mid] > arr[mid + k] - x) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return arr.slice(left, left+k);
+}
+
